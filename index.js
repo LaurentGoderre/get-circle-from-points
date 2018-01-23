e7996c42f26df9ab5fce2da18ac3adc10da8b452
Laurent Goderre <laurent.goderre@gmail.com>
2018-01-23 12:22:27 -0500

    Initial commit

---
+++ index.js
@@ -0,0 +1,18 @@
+export function getCircleFromPoints(point1, point2, angle) {
+  const diffX = point2[0] - point1[0];
+  const diffY = point2[1] - point1[1];
+
+  const segmentLength = Math.sqrt(
+    diffX ** 2 + diffY ** 2
+  );
+
+  const radius = (segmentLength / 2) / Math.sin(angle / 2);
+  const angle2 = (Math.PI / 2) - (Math.asin(diffY / segmentLength)) - ((Math.PI - angle) / 2);
+  const centerX = point1[0] + (Math.sin(angle2) * radius) * (diffX / Math.abs(diffX));
+  const centerY = point1[1] + Math.cos(angle2) * radius;
+  return {
+    r: radius,
+    cx: centerX,
+    cy: centerY
+  }
+}
