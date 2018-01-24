f509e51165eaf814b7e24fecb8fbd28a7a018aa5
Laurent Goderre <laurent.goderre@gmail.com>
2018-01-24 11:05:36 -0500

    Added a better fix for divisions by 0

--- index.js
+++ index.js
@@ -8,12 +8,9 @@ export function getCircleFromPoints(point1, point2, angle) {
 
   const radius = (segmentLength / 2) / Math.sin(angle / 2);
   const angle2 = (Math.PI / 2) - (Math.asin(diffY / segmentLength)) - ((Math.PI - angle) / 2);
-  let centerX = point1[0] + (Math.sin(angle2) * radius);
+  const centerX = point1[0] + (Math.sin(angle2) * radius) * (diffX !== 0 ? diffX / Math.abs(diffX) : 1);
   const centerY = point1[1] + Math.cos(angle2) * radius;
 
-  if (diffX !== 0) {
-    centerX *= (diffX / Math.abs(diffX));
-  }
   return {
     r: radius,
     cx: centerX,
